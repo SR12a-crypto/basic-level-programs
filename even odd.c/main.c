7345f8cb83a8893702ceb8373fefafdb7b08edec
SR12a <supreethreddypalla28n@gmail.com>
2026-09-15 23:51:42 +0530

    Add even odd program

---
+++ even odd.c/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+
+    a = 5;
+    b = 5;
+    sum = a + b;
+     if(sum%2==0)
+     {
+         printf("The sum is even:");
+     }
+     else
+     {
+         printf("The sum is odd:");
+     }
+     return 0;
+
+}
